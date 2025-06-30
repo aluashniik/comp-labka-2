a3b561d4f92d02198eae86503248933676f0b957
aluashniik <144539773+aluashniik@users.noreply.github.com>
2025-06-30 13:52:11 +0900

    add

---
+++ 4주차/fourNor.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/19/2024 03:57:18 PM
+// Design Name: 
+// Module Name: inv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module inv(
+
+input a,b,c,d,
+output e,f,g
+
+    );
+assign e=~(a&b);
+assign f=~(e&c);
+assign g=(~f&d);
+endmodule
